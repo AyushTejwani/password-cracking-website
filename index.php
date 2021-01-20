5c219216d5450772feb31b4150d83068fcd27478
AyushTejwani <38227488+AyushTejwani@users.noreply.github.com>
2021-01-20 14:19:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ echo $requestType;
 if($requestType=='POST')
 {
 
-	$time=exec(' cd ./cloud/ &&  time -p ./bin/serial-cracker -bluns -p $(echo -n 'test' | sha256sum)  2>&1 &');
+	$time=exec(' cd ./cloud/ &&  time -p ./bin/serial-cracker -bluns -p $(echo -n "test" | sha256sum)  2>&1 &');
 	$cores=1;
 	$strength='Good';
 }
