4cf6b33c8a81ce517b50c2f6c4215f5d934bebdc
AyushTejwani <38227488+AyushTejwani@users.noreply.github.com>
2021-02-05 16:32:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ if($requestType=='POST')
     <div class="container-fluid p-0">
       <div class="row no-gutters">
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/lock.jpeg');"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
           <h2>Improvements</h2>
           <p class="lead mb-0">
